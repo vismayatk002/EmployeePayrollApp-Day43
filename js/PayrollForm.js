2d8466a41ed983473b4b383e0e176a0ed9ba2568
vismayatk002 <vismayatk002@gmail.com>
2021-09-18 00:47:10 +0530

    Refactor displayed value of salary using event listner

---
+++ js/PayrollForm.js
@@ -0,0 +1,7 @@
+//UC8   
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input',function() {
+    output.textContent = salary.value;
+});
