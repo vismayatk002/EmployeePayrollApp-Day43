38eb7628b0aca40cacb1d9ebb4a34d75c121e482
vismayatk002 <vismayatk002@gmail.com>
2021-09-18 08:21:52 +0530

    Created object

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -5,3 +5,39 @@ output.textContent = salary.value;
 salary.addEventListener('input',function() {
     output.textContent = salary.value;
 });
+//UC9
+
+class Employee{
+
+    name;
+    img;
+    gender;
+    dept;
+    salary;
+    startDate;
+    notes;
+
+    set name(name){
+        this.name = name;
+    }
+    set img(img){
+        this.img = img;
+    }
+    set gender(gender){
+        this.gender = gender;
+    }
+    set dept(dept){
+        this.dept = dept;
+    }
+    set salary(salary){
+        this.salary = salary;
+    }
+    set startDate(startDate){
+        this.startDate = startDate;
+    }
+    set notes(notes){
+        this.notes = notes;
+    }
+}
+
+let emp = new Employee();
